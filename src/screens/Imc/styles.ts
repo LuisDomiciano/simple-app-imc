d4b13c721c8df3bfc34b10a43947319d69f3ba1f
LuisD <luis.domiciano@hotmail.com>
2026-01-11 15:00:13 -0300

    feat: styles screen

---
+++ src/screens/Imc/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  error: {
+    color: "red",
+    marginTop: 10,
+    textAlign: "center",
+  },
+  result: {
+    marginTop: 20,
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
